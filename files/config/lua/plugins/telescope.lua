5efca75540b6f37f5ef17a7a395b3ede160dfcef
Mailo Světel <mailo@rooland.cz>
2025-06-20 22:55:21 +0200

    [telescope] Chose options from menu

--- files/config/lua/plugins/telescope.lua
+++ files/config/lua/plugins/telescope.lua
@@ -1,9 +1,14 @@
 return {
   'nvim-telescope/telescope.nvim',
   tag = '0.1.8',
-  dependencies = { 'nvim-lua/plenary.nvim' },
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    "nvim-telescope/telescope-ui-select.nvim"
+  },
   config = function()
     local builtin = require('telescope.builtin')
+    require('telescope').load_extension("ui-select")
+
     vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
     vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
