c63e66e0c0f43fb62f8b2ce82637a3d37d57a3b4
Mailo Světel <mailo@rooland.cz>
2025-10-16 19:43:34 +0200

    [lsp] Config loading upgrade

--- files/config/lua/plugins/lsp-config.lua
+++ files/config/lua/plugins/lsp-config.lua
@@ -16,36 +16,32 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
-      local lspconfig = require("lspconfig")
-
-      require("config.diagnostics")
-
       -- Rubocop
-      lspconfig.rubocop.setup({})
+        vim.opt.signcolumn = "yes"
+        -- Original "LightBlue" for DiagnosticInfo wasn't readable for me
+        vim.api.nvim_set_hl(0, 'DiagnosticInfo', { fg = 'DarkGrey' })
+        vim.api.nvim_create_autocmd("FileType", {
+          pattern = "ruby",
+          callback = function()
+            local rubocop_cmd = vim.fn.system('which rubocop'):gsub('%s+', '')
 
-      vim.opt.signcolumn = "yes"
-      -- Original "LightBlue" for DiagnosticInfo wasn't readable for me
-      vim.api.nvim_set_hl(0, 'DiagnosticInfo', { fg = 'DarkGrey' })
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = "ruby",
-        callback = function()
-          local rubocop_cmd = vim.fn.system('which rubocop'):gsub('%s+', '')
-
-          vim.lsp.start {
-            name = "rubocop",
-            cmd = { rubocop_cmd, '--lsp' },
-          }
-        end,
-      })
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        pattern = "*.rb",
-        callback = function()
-          vim.lsp.buf.format()
-        end,
-      })
+            vim.lsp.start {
+              name = "rubocop",
+              cmd = { rubocop_cmd, '--lsp' },
+            }
+          end,
+        })
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          pattern = "*.rb",
+          callback = function()
+            vim.lsp.buf.format()
+          end,
+        })
+      --vim.lsp.config('rubocop')
+      vim.lsp.enable('rubocop')
 
       -- Steep
-      lspconfig.steep.setup({})
+      vim.lsp.enable('steep')
 
       vim.keymap.set('n', '<leader>J', vim.lsp.buf.hover, {})
       vim.keymap.set({ 'n' }, '<leader>ca', vim.lsp.buf.code_action, {})
